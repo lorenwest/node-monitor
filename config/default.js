ddfe73c1dde61e4b3c5ab4b60645baee1a9ee22a
Loren West <github@lorenwest.com>
2013-03-28 15:08:29 -0700

    Running from an app dir

--- config/default.js
+++ config/default.js
@@ -16,7 +16,11 @@ module.exports = {
     // accidental security breaches by requiring you to consider your network
     // security policies before allowing external connections.
     // See the external.js file in this directory for more information.
-    allowExternalConnections: false
+    allowExternalConnections: false,
+
+    // Path to the site database.  Paths that start with '.' are relative
+    // to process.cwd(), which is usually the application root directory.
+    siteDbPath: './site_db'
   }
 
 }
