73f85f58e32a81c19c9669d97e36dd20806f8c14
Loren West <email@lorenwest.com>
2013-11-14 22:48:48 -0800

    Squelched error logs for tests designed to error

---
+++ config/test.js
@@ -0,0 +1,10 @@
+// Configurations for running tests
+module.exports = {
+  Monitor: {
+
+    // Squelch log output so error tests aren't chatty
+    consoleLogListener: {
+      pattern: ""
+    }
+  }
+}
