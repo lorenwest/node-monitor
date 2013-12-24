035237c978bd2c52531c76c6f44aae9278141316
Loren West <email@lorenwest.com>
2013-12-23 23:19:30 -0800

    Back to gh-pages

--- test/0-Setup.js
+++
@@ -1,8 +0,0 @@
-// 0-Setup.js (c) 2010-2013 Loren West and other contributors
-// May be freely distributed under the MIT license.
-// For further details and documentation:
-// http://lorenwest.github.com/node-monitor
-
-// This should be run before other tests to set up configurations
-process.env.NODE_ENV='test';
-var config = require('config');
